79ccde6eaf87428012db4a15b017ffc959046774
Pruthvi G <pruthvialalliprivate@gmail.com>
2026-07-17 00:02:16 +0530

    Day 3

--- DSA/GreatestTwo.java
+++ DSA/GreatestTwo.java
@@ -4,5 +4,16 @@ public class GreatestTwo {
             Scanner sc = new Scanner(System.in);
 
             System.out.println("Enter Number 1: ");
-    }
+            int a = sc.nextInt();
+
+            System.out.println("Enter Number 2: ");
+            int  b = sc.nextInt();
+    
+            if(a>b){
+                        System.out.println("Greatest Number is: " +a);
+            }
+            else{
+                        System.out.println("Greatest Number is: "  +b);
+            }
+        }
 }
---
+++ DSA/primenumber.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+import javax.swing.plaf.synth.SynthStyle;
+public class primenumber {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter  a number: ");
+        int num=sc.nextInt();
+
+        int count=0;
+
+    for(int i=1;i<=num;i++){
+        if(num  % i == 0){
+            count++;
+       
+         } }
+
+        if(count==2){
+            System.out.println("Prime Number: ");
+        }
+        else{
+            System.out.println("Not a prime number");
+        }
+    }
+    
+}
